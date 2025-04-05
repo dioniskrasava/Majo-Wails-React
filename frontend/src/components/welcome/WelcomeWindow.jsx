8e0d13fb869861e026ea47349ab8e3aa89404c93
Ivan Belov <dionis.krasava@gmail.com>
2025-04-05 15:51:18 +0700

    0.0.12-r-2 стили (окно приветствия) доработал

--- frontend/src/components/welcome/WelcomeWindow.jsx
+++ frontend/src/components/welcome/WelcomeWindow.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React from "react";
+import "./style.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faM,
+  faA,
+  faJ,
+  faO,
+  faV,
+  fa0,
+  fa1,
+  fa2,
+  faHandPointDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 const WelcomeWindow = () => {
-
+  const sizeVersion = "1x";
   return (
-  <>
-    HELLO!
-  </>
+    <div className="welcome-container">
+      {" "}
+      {/* Новый контейнер */}
+      <div className="title-container">
+        {" "}
+        {/* Контейнер для букв */}
+        <span className="welcomeNameLabel underlined">
+          <FontAwesomeIcon icon={faM} size="4x" />
+        </span>
+        <span className="welcomeNameLabel">
+          <FontAwesomeIcon icon={faA} size="3x" />
+        </span>
+        <span className="welcomeNameLabel underlined">
+          <FontAwesomeIcon icon={faJ} size="4x" />
+        </span>
+        <span className="welcomeNameLabel">
+          <FontAwesomeIcon icon={faO} size="3x" />
+        </span>
+      </div>
+      <div className="version-container">
+        <span className="version-text">version 0.0.12</span>
+      </div>
+      <h1>Добро пожаловать</h1>
+      <p>Вы запустили Main Journal. Сокращенно - MaJo.</p>
+    </div>
   );
 };
 
